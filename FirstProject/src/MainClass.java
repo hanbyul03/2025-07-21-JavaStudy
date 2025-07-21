602673cb403f54ab6bb88810847bb2c56b92b3c2
sist <sist@sist27>
2025-07-21 14:47:15 +0900

    11

--- FirstProject/src/MainClass.java
+++
@@ -1,9 +0,0 @@
-//ctrl + f11 = 실행
-public class MainClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      System.out.println("Hello Java!!");  //ctrl+space
-      }
-
-}
